18102f799524c91fdcbea1e375534e8815b6488b
jqiu004 <jqiu004@ucr.edu>
2018-08-31 18:06:25 -0700

    First draft

---
+++ include/MicroSpec.hpp
@@ -0,0 +1,72 @@
+/*
+* @Author: Junqiao Qiu
+* @Last Modified by:   Junqiao Qiu
+* @Last Modified time: 2018-08-31 
+*/
+
+#ifndef MICROSPEC_H
+#define MICROSPEC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+namespace microspec 
+{
+	class  Input
+	{
+	public:
+		Input();	
+		Input(int* p, long size);	
+		~Input();
+
+		// @brief Provide @filename to access the target inputs  
+		static Input* ReadFromFile(char* filename);	
+		
+		int* getPointer() const;
+		long getLength() const;
+
+	private:
+
+		int* pointer;
+		long size;
+	};
+
+	class Table
+	{
+	public:
+		Table();
+		Table(int* List, int nState, int nSymbol, int start);
+		~Table();
+
+		static Table* ReadFromFile(char* filename);
+
+		int* getTable() const;
+		int getNumState() const;
+		int getNumSymbol() const;
+		int getStartState();
+
+	private:
+		int* tableList;
+		int numState;
+		int numSymbol;
+		int start;
+	};
+
+	class DFA
+	{
+	public:
+		DFA();
+		~DFA();
+		
+		void run(const Table* table, const Input* input);
+
+	private:
+		int* mTable;
+		int* mInputs;
+		int mStart;
+
+	};
+
+}	// end of namespace microspec
+
+#endif // MICROSPEC_H
---
+++ src/MicroSpec.cpp
@@ -0,0 +1,11 @@
+#include <>
+#include <>
+#include "MicroSpec.hpp"
+
+using namespace std;
+using namespace microspec;
+
+
+
+
+ 
